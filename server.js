e513501c37bb468ea24d362374b7af2d840adeda
EdaDuan <308506994@qq.com>
2021-09-13 11:51:28 +0800

    feat: 测试打印消息

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * @Autor: duanfy
  * @Date: 2021-09-08 18:45:47
  * @LastEditors: duanfy
- * @LastEditTime: 2021-09-09 09:55:26
+ * @LastEditTime: 2021-09-13 11:49:32
  */
 'use strict'
 const express = require('express')
@@ -14,6 +14,7 @@ const HOST = '127.0.0.1'
 // App
 const app = express()
 app.get('/', (req, res) => {
+  console.log(123);
   res.send('Hello world\n')
 })
 app.listen(PORT, HOST)
