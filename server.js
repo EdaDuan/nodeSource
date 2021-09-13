4e5fd56a41f470d83dc7ed441b570c31d4b8af9e
EdaDuan <308506994@qq.com>
2021-09-13 12:14:34 +0800

    feat; test第一次提交

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * @Autor: duanfy
  * @Date: 2021-09-08 18:45:47
  * @LastEditors: duanfy
- * @LastEditTime: 2021-09-13 11:49:32
+ * @LastEditTime: 2021-09-13 12:13:18
  */
 'use strict'
 const express = require('express')
@@ -14,7 +14,7 @@ const HOST = '127.0.0.1'
 // App
 const app = express()
 app.get('/', (req, res) => {
-  console.log(123);
+  res.send("123")
   res.send('Hello world\n')
 })
 app.listen(PORT, HOST)
